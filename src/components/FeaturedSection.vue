41aa9a811acd33898ab99742bb05758bca592a39
User01 <romansochirca30@gmail.com>
2022-10-25 15:35:02 +0300

    Fix FeaturedSection

--- src/components/FeaturedSection.vue
+++ src/components/FeaturedSection.vue
@@ -6,61 +6,9 @@
         <div class="featured__title noselect">Featured works</div>
 
         <div class="featured__cards">
-
-          <div class="featured__card">
-            <a href="#!" class="feature__card-link">
-              <div class="featured__card-wrapper">
-                <img src="../../featured_work/img_01.jpg" alt="" class="featured__card-img">
-              </div>
-            </a>
-            <div class="featured__card-content">
-              <h3 class="featured__card-title">Designing Dashboards</h3>
-              <div class="featured__card-information">
-                <span class="featured__information-date noselect">2020</span>
-                <span class="featured__information-tag noselect">Dashboard</span>
-              </div>
-              <p class="featured__card-description">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-            </div>
-          </div>
-
-          <div class="featured__card-line"></div>
-
-          <div class="featured__card">
-            <a href="#!" class="feature__card-link">
-              <div class="featured__card-wrapper">
-                <img src="../../featured_work/img_02.jpg" alt="" class="featured__card-img">
-              </div>
-            </a>
-            <div class="featured__card-content">
-              <h3 class="featured__card-title">Vibrant Portraits of 2020</h3>
-              <div class="featured__card-information">
-                <span class="featured__information-date noselect">2018</span>
-                <span class="featured__information-tag noselect">Illustration</span>
-              </div>
-              <p class="featured__card-description">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-            </div>
-          </div>
-
-          <div class="featured__card-line"></div>
-
-          <div class="featured__card">
-            <a href="#!" class="feature__card-link">
-              <div class="featured__card-wrapper">
-                <img src="../../featured_work/img_03.jpg" alt="" class="featured__card-img">
-              </div>
-            </a>
-            <div class="featured__card-content">
-              <h3 class="featured__card-title">36 Days of Malayalam type</h3>
-              <div class="featured__card-information">
-                <span class="featured__information-date noselect">2018</span>
-                <span class="featured__information-tag noselect">Typography</span>
-              </div>
-              <p class="featured__card-description">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
-            </div>
-          </div>
-
-          <div class="featured__card-line"></div>
-
+          <WorkCards
+            :cards="cards"
+          />
         </div>
 
       </div>
@@ -69,9 +17,52 @@
 </template>
 
 <script>
-  export default {
-    
+import WorkCards from './WorkCards.vue';
+
+export default {
+  components: {
+    WorkCards
+  },
+  data() {
+    return {
+      cards: [
+        {
+          id: 0,
+          imgName: "img_01",
+          title: "Designing Dashboards",
+          year: "2020",
+          tag: "Dashboard",
+          text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+        },
+        {
+          id: 1,
+          imgName: "img_02",
+          title: "Vibrant Portraits of 2020",
+          year: "2018",
+          tag: "Illustration",
+          text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+        },
+        {
+          id: 2,
+          imgName: "img_03",
+          title: "36 Days of Malayalam type",
+          year: "2018",
+          tag: "Typography",
+          text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+        },
+        {
+          id: 3,
+          imgName: "img_04",
+          title: "Components",
+          year: "2018",
+          tag: "Components, Design",
+          text: "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
+        },
+      ]
+    }
   }
+}
+
 </script>
 
 <style scoped>
@@ -80,60 +71,6 @@
   font-size: 22px;
   line-height: 60px;
   margin: 15px 0px 22px;
-}
-
-.featured__card {
-  display: flex;
-  justify-content: space-between;
-  transition: 0.2s all;
-}
-
-.featured__card-wrapper {
-  margin-right: 18px;
-  padding: 0 15px;
-}
-
-.featured__card-img {
-  border-radius: 6px;
-}
-
-.featured__card:hover {
-  transform: scale(1.05);
-}
-
-.featured__card-title {
-  font-weight: 700;
-  font-size: 30px;
-  line-height: 44px;
-  margin-top: -10px;
-}
-
-.featured__information-date {
-  padding: 1px 10px;
-  background: #142850;
-  border-radius: 16px;
-  color: #FFFFFF;
-  font-weight: 900;
-  font-size: 18px;
-  line-height: 60px;
-  margin-right: 26px;
-}
-
-.featured__information-tag {
-  font-size: 20px;
-  line-height: 29px;
-  color: #8695A4;
-}
-
-.featured__card-description {
-  font-size: 16px;
-  line-height: 24px;
-}
-
-.featured__card-line {
-  border-top: 1px solid #E0E0E0;
-  max-width: 100%;
-  margin: 15px 0px 35px;
 }
 /* END FEATURED */
 </style>
