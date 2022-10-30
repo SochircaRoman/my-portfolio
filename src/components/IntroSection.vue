b396ef8a8b3452ce405504659123de0c0e5c0653
User01 <romansochirca30@gmail.com>
2022-10-30 16:26:56 +0200

    Add My Tech Stack section

--- src/components/IntroSection.vue
+++ src/components/IntroSection.vue
@@ -7,8 +7,9 @@
           <div class="intro__content">
             <h1 class="intro__content-title">Hi, I am Roman, Junior Web Developer</h1>
             <p class="intro__content-subtitle">
-              My name is Roman and I am a junior web developer that create new websites and applications. I work with VueJs framework, I know: HTML, CSS, JS, Node.js, Python, I work in Visual Studio Code, Pycharm.
+              Hello! My name is Roman and I am a junior web developer that create new websites and web applications. I work with VueJs framework, I know: HTML, CSS, JS, Node.js, Python, I work in Visual Studio Code, Pycharm.
             </p>
+
             <button class="intro__content-btn">
               <a href="./download/roman_cv.pdf" target="_blank" class="intro__content-link">Download Resume</a>
             </button>
@@ -17,8 +18,56 @@
           <div class="intro__image">
             <img class="intro__image-photo" src="../assets/image_intro.jpg" alt="My image">
           </div>
-
         </div>
+
+
+        <div class="intro__content-tech">
+          <h2 class="intro__tech-title">My Tech Stack</h2>
+          <div class="intro__tech-content">
+            <div class="tech__content-menu">
+
+              <div class="tech__menu-content noselect">
+                <span class="tech__menu-line"></span>
+                <span class="tech__menu-name">frontend</span>
+              </div>
+              <div class="tech__menu-content noselect">
+                <span class="tech__menu-line"></span>
+                <span class="tech__menu-name">backend</span>
+              </div>
+
+              <div class="tech__menu-content noselect">
+                <span class="tech__menu-line"></span>
+                <span class="tech__menu-name">tools</span>
+              </div>
+
+            </div>
+            <div class="tech__content-skills">
+
+              <span class="skill__wrapper noselect">
+                <span><img class="skill__img" src="../../skills/css.png" alt=""></span>
+              </span>
+
+              <span class="skill__wrapper noselect">
+                <span><img class="skill__img" src="../../skills/node.png" alt=""></span>
+              </span>
+
+              <span class="skill__wrapper noselect">
+                <span><img class="skill__img" src="../../skills/vuejs.png" alt=""></span>
+              </span>
+
+              <span class="skill__wrapper noselect">
+                <span><img class="skill__img" src="../../skills/html5.png" alt=""></span>
+              </span>
+
+              <span class="skill__wrapper noselect">
+                <span><img class="skill__img" src="../../skills/js.png" alt=""></span>
+              </span>
+
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </section>
 
@@ -88,6 +137,66 @@ export default {
 
 .intro__image-photo {
   border-radius: 50%;
+}
+
+
+
+.intro__content-tech{
+  margin-bottom: 40px;
+}
+
+.intro__tech-title {
+  font-weight: 600;
+  font-size: 34px;
+  line-height: 60px;
+  color: #21243D;
+  margin-bottom: 20px;
+}
+
+.tech__content-menu {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
+
+.tech__menu-content:hover {
+  cursor: pointer;
+  transition: 0.2s all;
+  background:#b7b3b3;
+  border-left: 5px solid #21243D;
+}
+
+.tech__menu-line {
+  border-left: 1px solid #000000;
+  position: absolute;
+  height: 60px;
+}
+
+.tech__menu-name {
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 60px;
+  color: #21243D;
+  margin-left: 10px;
+}
+
+.tech__content-skills {
+  text-align: center;
+}
+
+.skill__img {
+  width: 60px;
+  height: 60px;
+  transition: 0.2s all;
+}
+.skill__img:hover {
+  transform: scale(1.1);
+}
+
+.skill__wrapper {
+  padding: 30px 10px;
+  margin-right: 10px;
 }
 /* END INTRO */
 </style>
