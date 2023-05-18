52194e7ba0c45f8145c5e264cd4ac439687184f5
User01 <romansochirca30@gmail.com>
2023-05-18 20:58:04 +0300

    Add SEO

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import ErrorPage from '../views/ErrorPage.vue'
 import PostDetail from '../components/UI/PostDetail.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
